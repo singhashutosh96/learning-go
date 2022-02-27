94ec890a0cb8c37a069d6992918dd6493c25a2c9
Ashutosh <s.ashutosh@hotmail.com>
2022-02-27 12:48:01 +0530

    first commit

---
+++ card/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var card string = "Ace of Spade"
+	// var card = "Ace of Spade"
+	card := "Ace of Spade"
+	card = "Five of Diamond"
+	fmt.Println(card)
+}
